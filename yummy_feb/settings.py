693e99736c04737e8c50886ac0686c787426c834
OST <cpython.projects@gmail.com>
2025-03-19 20:37:57 +0300

    django 10

--- yummy_feb/settings.py
+++ yummy_feb/settings.py
@@ -153,9 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 LOGIN_URL = 'account/login/'
